20d1ef56bee3504f252ea504e4546ef4fe3149a3
Sergey Okatov <sokatov@gmail.com>
2024-08-01 00:23:21 +0500

    M4l3 testing

--- lessons/m4l3-testing/src/commonTest/kotlin/KotestWithBDD.kt
+++ lessons/m4l3-testing/src/commonTest/kotlin/KotestWithBDD.kt
@@ -1,12 +1,16 @@
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 
 class KotestWithBDD() : BehaviorSpec({
     Given("State A") {
+        val state = "State A"
         println("state A ")
         When("Action A") {
+            val condition = 2
             println("in action A")
             Then("State => A1") {
                 println("becomes A1")
+                condition shouldBe 2
             }
         }
         When("Action B") {
